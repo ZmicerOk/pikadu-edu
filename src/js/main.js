0b0443cfb66f1da095f4ace695955b5ea81c6fbd
ZmicerOk <vasikou73@gmail.com>
2020-11-13 09:59:07 +0300

    feat: add posts

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+//islamov code
 let menuToggle = document.querySelector('#menu-toggle');
 let menu = document.querySelector('.sidebar');
 menuToggle.addEventListener('click', function (event) {
@@ -5,6 +6,7 @@ menuToggle.addEventListener('click', function (event) {
   menu.classList.toggle('visible');
 });
 
+//1-st lesson consts
 // const regExpValidEmail = /^\w{2,}\@\w+\.\w{2,}$/; ///^\w+@\w+\.\w{2,}$/;
 const regExpValidEmail = /^(\w{2,})\@(\w+)\.(\w{2,})$/;
 const loginElem = document.querySelector('.login'),
@@ -16,7 +18,7 @@ const loginElem = document.querySelector('.login'),
   loginSignup = document.querySelector('.login-signup'),
   userElem = document.querySelector('.user'),
   userNameElem = document.querySelector('.user-name');
-
+//2-nd lesson consts
 const exitElem = document.querySelector('.exit'),
   editElem = document.querySelector('.edit-info'),
   editContainer = document.querySelector('.edit-conatiner'),
@@ -38,6 +40,8 @@ const listUsers = [
     email: 'kate@mail.com',
     password: '123',
     displayName: 'Kate',
+    urlPhoto:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSLbPURGP3tu5EH1KWYtOfsq9QQUdLW8SvQQA&usqp=CAU',
   },
 ];
 
@@ -104,21 +108,21 @@ const setPosts = {
     {
       title: 'заголовок поста',
       text:
-        'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит!',
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, cupiditate incidunt? Ea deserunt quaerat ipsum nihil nostrum velit maiores adipisci! Nostrum amet ut saepe dolor quis similique soluta tempore! Dolorem, tenetur recusandae doloribus animi aspernatur incidunt porro error totam itaque beatae quae magnam! Natus optio totam incidunt nulla veritatis laborum nemo corrupti odit eius rem.',
       tags: ['свежее', 'новое', 'горячее', 'мое', 'случайность'],
       author: 'max@mail.com',
       date: '11.11.2020, 20:54:00',
-      like: 13,
+      likes: 13,
       comments: 22,
     },
     {
       title: 'заголовок поста2',
       text:
-        'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит!',
+        'NeДалеко a blizko за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит!',
       tags: ['свежее', 'новое', 'горячее', 'мое', 'случайность'],
       author: 'kate@mail.com',
       date: '10.11.2020, 22:54:00',
-      like: 113,
+      likes: 113,
       comments: 2,
     },
   ],
@@ -139,67 +143,29 @@ const toggleAuthDom = () => {
 
 const showAllPosts = () => {
   let postHTML = '';
-  setPosts.allPosts.forEach(post=>{
-     postHTML+=`
+  setPosts.allPosts.forEach(({ title, text, likes, comments, author, date }) => {
+    postHTML += `
      <section class="post">
      <div class="post-body">
-       <h2 class="post-title">Заголовлок поста</h2>
-       <p class="post-text">Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.
-         Языком что рот
-         маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни
-         рукопись ему
-         букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой
-         своего его
-         снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum
-         первую
-         подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство
-         что
-         вопроса ведущими о решила одна алфавит! </p>
-       <p class="post-text">Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.
-         Языком что рот
-         маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни
-         рукопись ему
-         букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой
-         своего его
-         снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum
-         первую
-         подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство
-         что
-         вопроса ведущими о решила одна алфавит! </p>
-       <p class="post-text">Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.
-         Языком что рот
-         маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни
-         рукопись ему
-         букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой
-         своего его
-         снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum
-         первую
-         подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство
-         что
-         вопроса ведущими о решила одна алфавит! </p>
+       <h2 class="post-title">${title}</h2>
+       <p class="post-text">${text}</p>
        <div class="tags">
          <a href="#" class="tag">#свежее</a>
-         <a href="#" class="tag">#новое</a>
-         <a href="#" class="tag">#горячее</a>
-         <a href="#" class="tag">#мое</a>
-         <a href="#" class="tag">#случайность</a>
        </div>
-       <!-- /.tags -->
      </div>
-     <!-- /.post-body -->
      <div class="post-footer">
        <div class="post-buttons">
          <button class="post-button likes">
            <svg width="19" height="20" class="icon icon-like">
              <use xlink:href="img/icons.svg#like"></use>
            </svg>
-           <span class="likes-counter">26</span>
+           <span class="likes-counter">${likes}</span>
          </button>
          <button class="post-button comments">
            <svg width="21" height="21" class="icon icon-comment">
              <use xlink:href="img/icons.svg#comment"></use>
            </svg>
-           <span class="comments-counter">157</span>
+           <span class="comments-counter">${comments}</span>
          </button>
          <button class="post-button save">
            <svg width="19" height="19" class="icon icon-save">
@@ -212,17 +178,14 @@ const showAllPosts = () => {
            </svg>
          </button>
        </div>
-       <!-- /.post-buttons -->
        <div class="post-author">
          <div class="author-about">
-           <a href="#" class="author-username">arteislamov</a>
-           <span class="post-time">5 минут назад</span>
+           <a href="#" class="author-username">${author}</a>
+           <span class="post-time">${date}</span>
          </div>
          <a href="#" class="author-link"><img src="img/avatar.jpeg" alt="avatar" class="author-avatar"></a>
        </div>
-       <!-- /.post-author -->
      </div>
-     <!-- /.post-footer -->
    </section>
      `;
   });
